3cbb90e26c619500fc3bde8f01e7131b5ae3e9ff
liweisheng <liweisheng@zuoshouyisheng.cn>
2019-07-10 10:04:48 +0800

    提交代码

--- js/banner.js
+++ js/banner.js
@@ -1,32 +1,34 @@
 (function () {
     window.onload = function () {
         var banner = document.querySelector("#banner-wrap");
-        var list = document.querySelector("#banner-wrap>#banner-list");
+        var list = document.querySelector("#banner-list");
         var rootWidth = -document.documentElement.clientWidth;
-        var startX = 0;
+        var startX = 0;//开始滑动的起始坐标x
+        var startY = 0;//开始滑动的起始坐标y
         var index = 0;
         var translateX = 0;
         var tid = null;
         var lis = null;
         var points = null;
-
+        var initWidth = 0;
         init();
-        autoPlay();
+        // autoPlay();
 
         //手指按下
         banner.addEventListener('touchstart', function (ev) {
             //禁止浏览器默认滑动事件
             ev.preventDefault();
-
+            console.log(6)
             startX = ev.changedTouches[0].clientX;
+            startY = ev.changedTouches[0].clientY;
 
             //关闭过渡效果
             list.style.transition = "0s";
-
             //关闭自动轮播
             clearInterval(tid);
 
             //无缝滑屏 改变index
+            console.log(list.style.transform,'样式');
             changeIndex();
         });
 
@@ -34,29 +36,46 @@
         banner.addEventListener('touchmove', function (ev) {
             //滑动差值
             var dis = ev.changedTouches[0].clientX - startX;
+            startX = ev.changedTouches[0].clientX;
+            var disY = ev.changedTouches[0].clientY - startY;
+            startY = ev.changedTouches[0].clientY;
 
-            //当前list总平移长度
-            translateX = rootWidth * index + dis;
+            if(Math.abs(dis)>Math.abs(disY)){
+                console.log('左右');
+                // ev.preventDefault();
+                //当前list总平移长度
+                // alert(33)
+                translateX = rootWidth * index + dis + initWidth;
+                initWidth += dis;
+                list.style.transform = "translateX(" + translateX + "px)";
+                console.log(startX,'startX',ev.changedTouches[0].clientX,dis,startY,ev.changedTouches[0].clientY,disY  );
+                //响应list滑动平移
+                //changePage(0, translateX);
+            }else{
+                console.log('上下');
+            }
 
-            //响应list滑动平移
-            changePage(0, translateX);
         });
 
         //手指抬起
         banner.addEventListener('touchend', function () {
             //根据滑动长度求索引
             index = Math.round(translateX / rootWidth);
-
+            console.log(index ,translateX);
+            initWidth = 0;
             //越界判断
             if (index < 0) {
                 index = 0;
+
             } else if (index > lis.length - 1) {
                 index = lis.length - 1;
             }
-
+            console.log( index,list.style.transform,'结束',rootWidth * index);
             changePage(.3, rootWidth * index);
             changePoint();
-            autoPlay();
+            changeIndex();
+
+            //autoPlay();
         });
 
         //自动轮播
@@ -78,7 +97,7 @@
 
         //更改索引 实现无缝滑屏
         function changeIndex() {
-            console.log(index,'index1')
+
             if (index == 0) {
                 //当显示第一张图片 切换到下一组的第一张
                 index = points.length;
@@ -86,7 +105,7 @@
                 //当显示最后一张 切换到上一组的最后一张
                 index = points.length - 1;
             }
-            console.log(index,'index2')
+
         }
 
         //设置小圆点
@@ -101,25 +120,24 @@
         function changePage(duration, translateVal) {
             list.style.transition = duration + "s";
             list.style.transform = "translateX(" + translateVal + "px)";
-            console.log(list.style,'style')
         }
 
         //初始化list和point
         function init() {
-            console.log(list,list.innerHTML,'list')
+
             //添加一组
             list.innerHTML += list.innerHTML;
 
             //设置list宽度
-            lis = document.querySelectorAll("#banner-wrap>#banner-list li");
-            console.log(lis,lis[0].style.width,'lis')
+            lis = document.querySelectorAll("#banner-list li");
+
             list.style.width = lis.length + "00%";
 
             //设置li宽度
             for (var i = 0; i < lis.length; i++) {
                 lis[i].style.width = 100 / lis.length + "%";
             }
-            console.log(lis,lis[0].style.width,'lis')
+
             //创建point div
             var pointDiv = document.createElement("div");
             pointDiv.setAttribute("id", "point");
@@ -132,7 +150,7 @@
             }
 
             banner.appendChild(pointDiv);
-            points = document.querySelectorAll("#banner-wrap>#point span");
+            points = document.querySelectorAll("#point span");
         }
     };
 })();
